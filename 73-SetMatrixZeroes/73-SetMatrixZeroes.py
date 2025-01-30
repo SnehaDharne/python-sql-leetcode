5a0e6a100300593e6b14da707c2f2fb57f220fd5
Sneha Dharne <55712542+SnehaDharne@users.noreply.github.com>
2025-01-30 17:08:49 -0500

    Update file 1/30/2025, 5:08:49 PM

--- 73-SetMatrixZeroes/73-SetMatrixZeroes.py
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.py
@@ -3,21 +3,49 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        row_num = set()
-        col_num = set()
+        # row_num = set()
+        # col_num = set()
+        # m = len(matrix)
+        # n = len(matrix[0])
+        # for i in range(m):
+        #     for j in range(n):
+        #         if matrix[i][j] == 0:
+        #             row_num.add(i)
+        #             col_num.add(j)
+        # print(row_num,col_num)
+
+        # for i in range(m):
+        #     if i in row_num:
+        #         matrix[i]=[0]*n
+        #         print(matrix)
+        #     for j in range(n):
+        #         if j in col_num:
+        #             matrix[i][j] = 0
+
+
         m = len(matrix)
         n = len(matrix[0])
+        col0 = 1
+
         for i in range(m):
             for j in range(n):
                 if matrix[i][j] == 0:
-                    row_num.add(i)
-                    col_num.add(j)
-        print(row_num,col_num)
-
-        for i in range(m):
-            if i in row_num:
-                matrix[i]=[0]*n
-                print(matrix)
+                    matrix[i][0] = 0
+                    if j!=0:
+                        matrix[0][j] = 0
+                    else:
+                        col0 = 0
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j] != 0:
+                    if matrix[i][0] == 0 or matrix[0][j] == 0:
+                        matrix[i][j] = 0
+        
+        if matrix[0][0] == 0:
             for j in range(n):
-                if j in col_num:
-                    matrix[i][j] = 0
+                matrix[0][j] = 0
+        if col0 == 0:
+            for i in range(m):
+                matrix[i][0] = 0
+
+ 
