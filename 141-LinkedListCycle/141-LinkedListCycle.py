77c3a5f6ae24d8f7bec50315812a789f3972f302
Sneha Dharne <55712542+SnehaDharne@users.noreply.github.com>
2025-02-08 14:22:32 -0500

    Update file 2/8/2025, 2:22:32 PM

--- 141-LinkedListCycle/141-LinkedListCycle.py
+++ 141-LinkedListCycle/141-LinkedListCycle.py
@@ -7,13 +7,23 @@ class ListNode:
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
 
-        visited = []
-        while head != None:
-            if head not in visited:
-                visited.append(head)
-            else:
-                return True
-            head = head.next
+        # visited = []
+        # while head != None:
+        #     if head not in visited:
+        #         visited.append(head)
+        #     else:
+        #         return True
+        #     head = head.next
         
+        # return False
+
+        slow = head
+        fast = head
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+
         return False
         
