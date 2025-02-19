5194ca9bf616978e68e006f15173a1bf58013492
Sneha Dharne <55712542+SnehaDharne@users.noreply.github.com>
2025-02-18 20:45:53 -0500

    File updated at 2/18/2025, 8:45:53 PM

--- 151-ReverseWordsinaString/151-ReverseWordsinaString.py
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.py
@@ -1,10 +1,7 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
         words = s.split(' ')
-        print(words)
-        if len(words) == 1:
-            return s
         words = words[::-1]
-        res = [i for i in words if i != ''] 
-        ans = " ".join(res)
+        words = [i for i in words if i != ''] 
+        ans = " ".join(words)
         return ans
