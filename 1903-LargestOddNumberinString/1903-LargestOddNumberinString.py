51d78495fc70160815ecd54cc2ccb7b899ff06ef
Sneha Dharne <55712542+SnehaDharne@users.noreply.github.com>
2025-02-18 20:55:07 -0500

    File updated at 2/18/2025, 8:55:07 PM

--- 1903-LargestOddNumberinString/1903-LargestOddNumberinString.py
+++ 1903-LargestOddNumberinString/1903-LargestOddNumberinString.py
@@ -1,13 +1,12 @@
 def check_num(num):
-    number = int(num[-1])
-    if number % 2 != 0:
+    if int(num) % 2 != 0:
         return True
     else:
         return False
 class Solution:
     def largestOddNumber(self, num: str) -> str:
         for i in range(len(num)-1, -1, -1):
-            ans = check_num((num[:i+1]))
+            ans = check_num((num[i]))
             if ans:
                 return num[:i+1]
             else:
