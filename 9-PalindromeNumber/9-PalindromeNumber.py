d57d7d9141c472f93e8eb8dbb688d0b97e5c7aaa
Sneha Dharne <55712542+SnehaDharne@users.noreply.github.com>
2025-03-05 17:06:26 -0500

    Create file

---
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        num = str(x)
+        if len(num) == 1:
+            return True
+        i=0
+        j=len(num) -1
+        while i<j:
+            if num[i] != num[j]:
+                return False
+            i+=1
+            j-=1
+
+        return True
