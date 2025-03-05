91644fbe5d19f7c427cce41712307481a7d91d17
Sneha Dharne <55712542+SnehaDharne@users.noreply.github.com>
2025-03-05 17:07:41 -0500

    File updated at 3/5/2025, 5:07:41 PM

--- 9-PalindromeNumber/9-PalindromeNumber.py
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -4,15 +4,15 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        num = str(x)
-        if len(num) == 1:
-            return True
-        i=0
-        j=len(num) -1
-        while i<j:
-            if num[i] != num[j]:
-                return False
-            i+=1
-            j-=1
+        # num = str(x)
+        # if len(num) == 1:
+        #     return True
+        # i=0
+        # j=len(num) -1
+        # while i<j:
+        #     if num[i] != num[j]:
+        #         return False
+        #     i+=1
+        #     j-=1
 
-        return True
+        return str(x) == str(x)[::-1]
