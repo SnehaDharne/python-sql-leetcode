4a374ff58f430cd815030a50fb3c300ee70ba265
Sneha Dharne <55712542+SnehaDharne@users.noreply.github.com>
2025-03-12 00:14:36 -0400

    Create file

---
+++ 69-Sqrt(x)/69-Sqrt(x).py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x == 1:
+            return 1
+        if x == 0:
+            return 0
+        if x < 0:
+            return 0
+
+        start = 1
+        end = x
+
+        while start <= end:
+            mid = (start + end) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid > x:
+                end = mid - 1
+            else:
+                start = mid + 1
+
+        return end
+            
+        
+            
