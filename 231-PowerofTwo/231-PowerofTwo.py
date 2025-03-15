33b5501dc58bea2ec30074bf3f933bda801df1ed
Sneha Dharne <55712542+SnehaDharne@users.noreply.github.com>
2025-03-15 14:01:29 -0400

    Create file

---
+++ 231-PowerofTwo/231-PowerofTwo.py
@@ -0,0 +1,11 @@
+import math
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        else:
+            return  (n & (n - 1)) == 0
