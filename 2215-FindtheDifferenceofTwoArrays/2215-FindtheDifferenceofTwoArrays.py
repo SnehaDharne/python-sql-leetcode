d60709d57c8175e698f4e80901a6ef69c9ae2475
Sneha Dharne <55712542+SnehaDharne@users.noreply.github.com>
2025-04-05 17:29:33 -0400

    Create file

---
+++ 2215-FindtheDifferenceofTwoArrays/2215-FindtheDifferenceofTwoArrays.py
@@ -0,0 +1,9 @@
+# Last updated: 4/5/2025, 5:29:33 PM
+class Solution(object):
+    def findDifference(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[List[int]]
+        """
+        return ([list(set(nums1)-set(nums2)), list(set(nums2) - set(nums1))])
