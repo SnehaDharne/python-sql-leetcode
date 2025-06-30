16148526378823fce3ff8ccbe0977537345be27f
Sneha Dharne <55712542+SnehaDharne@users.noreply.github.com>
2025-06-30 08:39:06 -0400

    Create 202-HappyNumber

---
+++ 202-HappyNumber/202-HappyNumber.py
@@ -0,0 +1,16 @@
+# Last updated: 6/30/2025, 8:39:06 AM
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        hash_set = set()
+        while n != 1:
+            m = str(n)
+            sum1 = 0
+            for num in m:
+                sum1 += int(num) * int(num)
+            if sum1 in hash_set:
+                return False
+            else:
+                hash_set.add(sum1)
+            n = sum1
+
+        return True
